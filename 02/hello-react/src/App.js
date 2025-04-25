4b671b0d3d276c6b5607ab5f5fd8a7c319bcb858
DESKTOP-8LOGFNE\admin <phj2394324@naver.com>
2025-04-25 15:47:16 +0900

    first commit

---
+++ 02/hello-react/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+// import BasicJSX from './jsx_example/BasicJSX';
+// import JSXSyntaxRules from './jsx_example/JSXSyntaxRules';
+import JSXExpreesions from './jsx_example/JSXExpressions';
+
+function App() {
+  return <JSXExpreesions />;
+}
+
+export default App;
---
+++ 02/hello-react/src/jsx_example/BasicJSX.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function BasicJSX(){
+    // JSX를 사용한 선언
+    const element = <h1>Hello, Donur</h1>;
+
+    // JSX  없이 동일한 요소 생성
+    const elementWithoutJSX = React.createElement('h1', null, 'hello, Ddochi');
+
+    return(
+        <div>
+            {element}
+            <hr/>
+            {elementWithoutJSX}
+            <p>위 두 요소는 동일하게 렌더링됩니다.</p>
+        </div>
+    )
+
+}
+
+export default BasicJSX;
